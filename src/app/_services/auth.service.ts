4045844aac605c74ca0485ef0b1d0a3ac88c7af8
Fanik <fanikfinance@gmail.com>
2023-08-01 21:42:37 -0400

    first commit

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  // headers: new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Headers':
+  //     'Origin, X-Requested-With, Content-Type, Accept',
+  // }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+
+      {
+        username,
+
+        password,
+      },
+
+      httpOptions
+    );
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+
+      {
+        username,
+
+        email,
+
+        password,
+      },
+
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(AUTH_API + 'signout', {}, httpOptions);
+  }
+}
