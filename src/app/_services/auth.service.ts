50e91d3457ee6d266736f97a35d40e9064d7a9ab
Fanik <fanikfinance@gmail.com>
2023-08-01 22:49:24 -0400

    second try

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -8,13 +8,13 @@ import { Observable } from 'rxjs';
 const AUTH_API = 'https://fresh-server.onrender.com/api/auth/';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  // headers: new HttpHeaders({
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Origin': '*',
-  //   'Access-Control-Allow-Headers':
-  //     'Origin, X-Requested-With, Content-Type, Accept',
-  // }),
+  //headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  }),
 };
 
 @Injectable({
