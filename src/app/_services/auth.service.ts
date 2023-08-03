38127f97f882cbed923f68c26db46567dff593f2
Fanik <fanikfinance@gmail.com>
2023-08-02 22:46:56 -0400

    update config

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -9,12 +9,13 @@ const AUTH_API = 'https://fresh-server.onrender.com/api/auth/';
 
 const httpOptions = {
   //headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  headers: new HttpHeaders({
+  /*headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers':
       'Origin, X-Requested-With, Content-Type, Accept',
-  }),
+  }),*/
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   withCredentials: true,
 };
 
